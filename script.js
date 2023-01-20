89d0018c7cd0bfc99d7016827af6580814e5bece
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-01-20 11:11:53 +0300

    Add function to play a single round

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ function getComputerChoice() {
     let computerChoice = choice[randomNumber];
     return computerChoice;
 }
+
+// function to play single round
+
+function playRound(playerSelection, computerSelection) {
+    let text;
+
+    switch (true) {
+        case (playerSelection === 'rock' && computerSelection === 'paper'):
+            text = `You lose ${computerSelection} beats ${playerSelection}`;
+            break;
+        case (playerSelection === 'paper' && computerSelection === 'rock'):
+            text = 'You win! Paper beats Rock';
+            break;
+        case (playerSelection === 'rock' && computerSelection === 'scissors'):
+            text = 'You win! Rock crashes scissors';
+            break;
+        case (playerSelection === 'scissor' && computerSelection === 'rock'):
+            text = 'You lose! Rock beats scissors';
+            break;
+        case (playerSelection === 'scissor' && computerSelection === 'paper'):
+            text = 'You win! Scissors cut paper';
+            break;
+        case (playerSelection === 'paper' && computerSelection === 'scissors'):
+            text = 'You lose! Paper gets cut by scissors';
+            break;
+        case (playerSelection === computerSelection):
+            text = `Its a draw! You both chose ${playerSelection}`;
+            break;
+        default:
+            console.log('Something is not right');
+    }
+    return text;
+}
+
