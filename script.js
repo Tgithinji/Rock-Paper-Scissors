9f9d889086c27ecbcc60ff7f3c9fab5414f7a3b9
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-01-31 10:57:58 +0300

    Added a functioning GUI

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let playerScore = 0;
 let computerScore = 0;
-let draw = 0;
 
 // function to get computers choice
 
@@ -9,63 +8,80 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * choice.length);
     let computerChoice = choice[randomNumber];
     return computerChoice;
+
+}
+const main = document.getElementById('main');
+const images = document.querySelectorAll('img');
+const scoreFinal = document.querySelectorAll('.final-score');
+const divResults = document.createElement('div');
+const divFinal = document.createElement('div');
+divResults.classList.add('results');
+divFinal.classList.add('results');
+
+function disable () {
+    images.forEach(image => {
+        image.removeEventListener('click', () => {
+            playRound(image.alt.toLowerCase())
+        })
+    });
+}
+
+function reloadButton() {
+    const reloadBtn = document.createElement('button');
+    reloadBtn.textContent = 'Play Again';
+    reloadBtn.style.cssText = 'display:block; margin: 10px auto; padding: 10px 20px';
+    divFinal.appendChild(reloadBtn);
+    reloadBtn.addEventListener('click', () => {
+        window.location.reload();
+    })
 }
 
 // function to play single round
 
-function playRound(playerSelection, computerSelection) {
-    let text;
-
-    switch (true) {
-        case (playerSelection === 'rock' && computerSelection === 'paper'):
-        case (playerSelection === 'scissors' && computerSelection === 'rock'):
-        case (playerSelection === 'paper' && computerSelection === 'scissors'):
-            computerScore ++;
-            text = `You lose ${computerSelection} beats ${playerSelection}`;
-            break;
-
-        case (playerSelection === 'paper' && computerSelection === 'rock'):
-        case (playerSelection === 'scissors' && computerSelection === 'paper'):
-        case (playerSelection === 'rock' && computerSelection === 'scissors'):
-            playerScore ++;
-            text = `You win! ${playerSelection} beats ${computerSelection}`;
-            break;
-
-        case (playerSelection === computerSelection):
-            draw ++;
-            text = `Its a draw! You both chose ${playerSelection}`;
-            break;
-        default:
-            text = 'Oops something is not right';
-    }
-    alert(text);
-}
-
-// function to get playerSelection and check if valid
-
-function getPlayerSelection() {
-    let userInput = prompt('Rock, Paper or Scissors', '').toLowerCase();
-    if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper') {
-        return userInput;
-    } else {
-        alert('Please enter a valid answer!');
-    }
-}
-
-// function that plays 5 rounds of the game
-
-function game() {
-    let gamesPlayed = 0;
-    
-    for (let i = 0; i < 5; i ++) {
-        let  playerSelection = getPlayerSelection();
+function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        gamesPlayed ++;
-    }
-    alert(`You won ${playerScore} out of ${gamesPlayed} games
-    Computer won ${computerScore} out of ${gamesPlayed} games
-    ${draw} games ended as draw`);
+    
+    if ((playerSelection === 'scissors' && computerSelection === 'rock') || 
+    (playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors')) {
+            computerScore ++;
+            scoreFinal[1].textContent = computerScore; 
+            divResults.textContent = `${computerSelection} beats ${playerSelection}`;
+            main.appendChild(divResults);   
+
+            if (computerScore === 5) {
+                divFinal.textContent = `You loose. You scored ${playerScore} out of 5`;
+                
+                reloadButton();
+                main.appendChild(divFinal);
+                disable();
+            }
+                   
+    } else if ((playerSelection === 'paper' && computerSelection === 'rock') || 
+       (playerSelection === 'scissors' && computerSelection === 'paper') ||
+       (playerSelection === 'rock' && computerSelection === 'scissors')) {
+            playerScore ++
+            scoreFinal[0].textContent = playerScore; 
+            divResults.textContent = `${playerSelection} beats ${computerSelection}`;
+            main.appendChild(divResults);
+
+            if (playerScore === 5) {
+                divFinal.textContent = `You Win. You scored ${playerScore} out of 5`
+                
+                reloadButton();
+                main.appendChild(divFinal);
+                disable();
+            }
+    } else {
+            divResults.textContent = `You both choose ${playerSelection}`;
+            main.appendChild(divResults);
+    }    
 }
 
-game();
+images.forEach(image => {
+    
+    image.addEventListener('click', () => {
+        playRound(image.alt.toLowerCase());
+    });
+    
+});
